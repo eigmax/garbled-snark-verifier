54c30ad9e081e1142603a6ec96b5239037e69e36
Hakkush-07 <hakan.karakus@std.bogazici.edu.tr>
2025-05-30 12:22:09 +0300

    fq2, fq6, fq12 add mul as todo

---
+++ src/circuits/bn254/fq2.rs
@@ -0,0 +1,14 @@
+use crate::bag::*;
+
+pub struct Fq2;
+
+impl Fq2 {
+    pub fn add(input_wires: Vec<Rc<RefCell<Wire>>>) -> (Vec<Rc<RefCell<Wire>>>, Vec<Gate>) {
+        todo!()
+    }
+
+    pub fn mul(input_wires: Vec<Rc<RefCell<Wire>>>) -> (Vec<Rc<RefCell<Wire>>>, Vec<Gate>) {
+        todo!()
+    }
+}
+
---
+++ src/circuits/bn254/fq6.rs
@@ -0,0 +1,14 @@
+use crate::bag::*;
+
+pub struct Fq6;
+
+impl Fq6 {
+    pub fn add(input_wires: Vec<Rc<RefCell<Wire>>>) -> (Vec<Rc<RefCell<Wire>>>, Vec<Gate>) {
+        todo!()
+    }
+
+    pub fn mul(input_wires: Vec<Rc<RefCell<Wire>>>) -> (Vec<Rc<RefCell<Wire>>>, Vec<Gate>) {
+        todo!()
+    }
+}
+
